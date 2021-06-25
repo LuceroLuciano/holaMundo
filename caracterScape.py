9997f5bcd2a9cef0cb2e6db3647657abaec15865
LuceroLuciano <lucero.luciano@umb.edu.mx>
2021-06-25 15:55:19 -0500

    hola mundo

---
+++ caracterScape.py
@@ -0,0 +1,4 @@
+print ("***CARACTER DE SCAPE***")
+print("witsi witsi araña\nsubio su telaraña")
+print()
+print("vino la lluvia\ny se la llevo")
---
+++ helloWorld.py
@@ -0,0 +1,36 @@
+print("\n***ARGUMENTOS***")
+
+print()  #Funcion print() sin argumentos
+print("Hello!") #Función print() con un argumento "Hello!"
+print("Hello", "How was your day?", "I hope that you're ok.") #Función print() con tres argumrntos, separados por comas
+print("Hola","¿Como estubo tu dia?","Espero estes bien.")
+print('hola "lucero, como estas"',"bienvenida a Microsoft")
+
+
+print("\n***LA MANERA POSICIONAL DE PASAR ARGUMENTOS***")
+
+print("My name is","Python")
+print("Monty Python.")
+
+
+print("\n******ARGUMENTOS DE PALABRA CLAVE")
+print("Hello", "Luz", "Daylight", end=" ")
+print("Monty Python.")
+
+
+print("My","name","is","Monty", "Python", sep="-*-")
+
+print("\n***COMBINACIÓN DE ARGUMENTOS***")
+print("My","name","is",sep="-",end="*")
+print("Lucero","Luciano",sep="*",end="\n")
+
+print("\n*** LABORATORIO ***")
+print("Funadamentos","Programacion","en",sep="***",end="...")
+print("Python")
+
+print("\n\n")
+print("*** LABORATORIO 2***")
+
+print("     *\n   * *\n  *  *\n *   *")
+
+
